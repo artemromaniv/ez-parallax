b559ddf84f2e27e927d8bab45ac60d367db18bb7
artem <artemromaniv@yahoo.com>
2023-01-25 04:51:10 -0800

    add support for styling Parallax Components

--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -3,10 +3,12 @@ import React, { useState, useEffect, ReactNode } from 'react';
 interface Props {
   children: ReactNode;
   speed: number;
+  className?: string
 }
 
-const Parallax = ({ children, speed }: Props) => {
+const Parallax = ({ children, speed, className }: Props) => {
   const [scrollY, setScrollY] = useState(0);
+  const [scrollX, setScrollX] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -19,7 +21,7 @@ const Parallax = ({ children, speed }: Props) => {
   }, []);
 
   return (
-    <div style={{ transform: `translateY(${scrollY * speed}px)` }}>
+    <div style={{ transform: `translateY(${scrollY * speed}px)` }} className = {className}>
       {children}
     </div>
   );
--- src/components/ParallaxHorizontal.tsx
+++ src/components/ParallaxHorizontal.tsx
@@ -3,9 +3,10 @@ import React, { useState, useEffect, ReactNode } from 'react';
 interface Props {
   children: ReactNode;
   speed: number;
+  className?: string
 }
 
-const ParallaxHorizontal = ({ children, speed }: Props) => {
+const ParallaxHorizontal = ({ children, speed, className }: Props) => {
   const [scrollX, setScrollX] = useState(0);
 
   useEffect(() => {
@@ -19,7 +20,7 @@ const ParallaxHorizontal = ({ children, speed }: Props) => {
   }, []);
 
   return (
-    <div style={{ transform: `translateX(${scrollX * speed}px)` }}>
+    <div style={{ transform: `translateX(${scrollX * speed}px)` }} className = {className}>
       {children}
     </div>
   );
