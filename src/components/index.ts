8e944a350de82a8e686b650b77eeef54c18e7bc0
artem <artemromaniv@yahoo.com>
2023-01-30 07:02:51 -0800

    import ParallaxLayer component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export {default as Parallax} from './Parallax'
 export {default as ParallaxHorizontal} from './ParallaxHorizontal'
 export {default as FollowMouse} from './FollowMouse'
+export {default as ParallaxLayer} from './ParallaxLayer'
